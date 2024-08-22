bb676912272f8345c84ab1812b7cfa116f831f43
diego.krug <diego.krug@emiteai.com.br>
2024-08-22 15:46:50 -0300

    Criando Calculadora

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./index.css";
+import ReactDOM from "react-dom";
+import React from "react";
+import Calculator from "./main/Calculator";
+
+ReactDOM.render(
+  <>
+    <h1>Calculadora</h1>
+    <Calculator />
+  </>,
+  document.getElementById("root")
+);
